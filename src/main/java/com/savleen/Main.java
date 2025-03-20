f1c8c5a349f4126b716488e2a02e2c237e683ac9
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-21 02:15:30 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -4,4 +4,41 @@ public class Main {
     public static void main(String[] args) { 
         System.out.println("Hello SonarCloud!");
     }
+    public void useOfSystemOut() {
+        // ❌ System.out.println should be replaced with a logger (java:S106)
+        System.out.println("Logging should be done using a logger.");
+    }
+
+    public void switchWithoutDefault(int value) {
+        // ❌ Switch statement should have a default case (java:S131)
+        switch (value) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+        }
+    }
+
+    public void nestedIfStatements(int x) {
+        // ❌ Avoid too many nested if statements (java:S1067)
+        if (x > 0) {
+            if (x > 5) {
+                if (x > 10) {
+                    System.out.println("X is greater than 10");
+                }
+            }
+        }
+    }
+
+    public void emptyCatchBlock() {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            // ❌ Catch block should not be empty (java:S2486)
+        }
+    }
+
+
 }
