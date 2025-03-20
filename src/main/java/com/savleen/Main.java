e671c69eb011b25198219b988a9fa0131119787c
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-20 18:03:37 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -1,5 +1,6 @@
 package com.savleen;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) { 
         System.out.println("Hello SonarCloud!");
