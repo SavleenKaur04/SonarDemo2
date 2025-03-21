1e24d645cef839357843ec00226a4e2582c2fe93
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-21 17:55:55 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) { 
         System.out.println("Hello SonarCloud!");
     }
+    int x=78;
+     public void emptyCatchBlock() {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            // ❌ Catch block should not be empty (java:S2486)
+        }
+    }
 }
