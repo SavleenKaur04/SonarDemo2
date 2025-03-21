7e2918bf375b93fad6db4fb14bc9d51d024eaa23
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-21 21:14:49 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -5,4 +5,22 @@ public class Main {
         System.out.println("Hello SonarCloud!");
     }
     int x=19;
+     public void nestedIfStatements(int x) {
+        // ❌ Avoid too many nested if statements (java:S1067)
+        if (x > 0) {
+            if (x > 5) {
+                if (x > 10) {
+                    System.out.println("X is greater than 10");
+                }
+            }
+        }
+    }
+
+    public void emptyCatchBlock() {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            // ❌ Catch block should not be empty (java:S2486)
+        }
+    }
 }
