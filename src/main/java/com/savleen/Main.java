08de77c28fc80adc7e72cbd185e4c5755a01af7d
SavleenKaur04 <91909667+SavleenKaur04@users.noreply.github.com>
2025-03-23 05:52:39 +0530

    Update Main.java

--- src/main/java/com/savleen/Main.java
+++ src/main/java/com/savleen/Main.java
@@ -4,24 +4,15 @@ public class Main {
     public static void main(String[] args) { 
         System.out.println("Hello SonarCloud!");
 
-        var x = "fwegjkshfewukjshfukjs";
-         public void nestedIfStatements(int x) {
-        // ❌ Avoid too many nested if statements (java:S1067)
-        if (x > 0) {
-            if (x > 5) {
-                if (x > 10) {
-                    System.out.println("X is greater than 10");
-                }
-            }
+        public void switchWithoutDefault(int value) {
+        // ❌ Switch statement should have a default case (java:S131)
+        switch (value) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
         }
-    }
-
-    public void emptyCatchBlock() {
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            // ❌ Catch block should not be empty (java:S2486)
-        }
-    }
     }
 }
